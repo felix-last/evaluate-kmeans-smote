de7a0f0fd6e9626327ca07a488fff6def7716260
Felix Last <mail@felixlast.de>
2017-10-01 17:41:11 +0200

    get optimal hyperparameters through compute_optimal_stats

--- hyperparameters.py
+++ hyperparameters.py
@@ -46,6 +46,65 @@ best
 # display(res[(res['Dataset'] == 'iris') & (res['Oversampler'] == 'KMeansSMOTE99')])
 
 #%%
+# get optimal stats
+optimal = imbtools.evaluation.calculate_optimal_stats(binary_experiment, return_optimal_params=True)
+
+#%%
+# split oversampler and classifier column into to columns
+optimal['ClassifierKind'] = optimal['Classifier'].apply(lambda o: o[0])
+optimal['OversamplerKind'] = optimal['Oversampler'].apply(lambda o: o[0])
+optimal['ClassifierConfiguration'] = optimal['Classifier'].apply(lambda o: o[1])
+optimal['OversamplerConfiguration'] = optimal['Oversampler'].apply(lambda o: o[1])
+# filter only kmeans smote rows
+optimal = optimal[ optimal['OversamplerKind'] == 'KMeansSMOTE' ]
+# Remove unnecessary metrics
+optimal = optimal[ ~optimal['Metric'].isin(['tp','tn','fp','fn']) ]
+optimal
+
+#%%
+# create a well-readable table
+table_optimal = optimal.drop(['Oversampler','Classifier', 'OversamplerKind', 'OversamplerConfiguration', 'ClassifierConfiguration', 'Mean CV score', 'Std CV score'], axis=1)
+# add configuration of kmeans smote as columns
+table_optimal['Score'] = optimal['Mean CV score']
+table_optimal['SD'] = optimal['Std CV score']
+table_optimal['k'] = optimal['OversamplerConfiguration'].apply(lambda o: o['kmeans_args']['n_clusters'])
+table_optimal['knn'] = optimal['OversamplerConfiguration'].apply(lambda o: o['smote_args']['k_neighbors'])
+table_optimal['de'] = optimal['OversamplerConfiguration'].apply(lambda o: o['density_power'])
+table_optimal['irt'] = optimal['OversamplerConfiguration'].apply(lambda o: o['imbalance_ratio_threshold'])
+# add classifier configurations as columns
+# since we only have max 1 variable parameter per classifier, use only one column
+def get_classifier_param(o):
+    if 'n_estimators' in o:
+        return o['n_estimators']
+    elif 'n_neighbors' in o:
+        return o['n_neighbors']
+    else:
+        return np.nan
+table_optimal['Clf'] = optimal['ClassifierConfiguration'].apply(get_classifier_param)
+# empty classifier parameter when it's not applicable
+table_optimal['Clf'] = table_optimal['Clf'].replace(np.nan, '')
+# empty density exponent when it's not applicable
+table_optimal['de'] = table_optimal['de'].replace(np.nan, '')
+table_optimal = table_optimal.set_index(['Dataset','ClassifierKind','Metric'])
+table_optimal = table_optimal.unstack('Metric')
+table_optimal = table_optimal.swaplevel(0,1, axis=1)
+table_optimal.sortlevel(0, axis=1, inplace=True)
+# replace infs with string to allow rounding
+table_optimal = table_optimal.replace([np.inf, -np.inf], '∞')
+table_optimal = table_optimal.round(2)
+# choose only one metric, otherwise they are too many
+table_optimal = table_optimal['average_precision']
+table_optimal
+#%%
+# export
+print(table_optimal.to_latex())
+
+# %%
+# get table of datasets
+datasets = imbtools.evaluation.read_csv_dir(cfg['dataset_dir'])
+print(imbtools.evaluation.summarize_datasets(datasets).to_latex())
+
+#%%
 # Look at one metric and plot the various oversamplers
 pr_res = res[res['Metric'] == 'average_precision']
 # look at only one classifier
