a8290ea5fd4e5e3ff1586944c288c8d6661836b9
Felix Last <mail@felixlast.de>
2017-10-02 20:28:46 +0200

    Sensitivity analysis for multiple classifiers at once

--- hyperparameters.py
+++ hyperparameters.py
@@ -142,50 +142,101 @@ pd.DataFrame(binary_experiment.oversamplers_)
 # %%
 # Sensititvity Analysis
 
-# parameters for the analysis
-classifier = 'LogisticRegression1'
-metric = 'geometric_mean_score'
-variable_param = 'k'
-fixed_params = {
-    'de': None,
-    'knn': 3,
-    'irt': 1
-}
+def sensitivity_analysis(classifiers, metric, variable_param, fixed_params, as_latex=True):
+    variable_param, variable_param_type = variable_param
+    metric, metric_name = metric
+    # find all kmeanssmote instances which correspond to the fixed params
+    oversampler_names, oversamplers = zip(*binary_experiment.oversamplers_)
+    oversamplers_dict = {}
+    for i, o in enumerate(oversamplers):
+        try:
+            if o.kmeans_args:
+                oversamplers_dict[oversampler_names[i]] = {}
+                oversamplers_dict[oversampler_names[i]]['k'] = o.kmeans_args['n_clusters']
+                oversamplers_dict[oversampler_names[i]]['de'] = o.density_power
+                oversamplers_dict[oversampler_names[i]]['irt'] = o.imbalance_ratio_threshold
+                oversamplers_dict[oversampler_names[i]]['knn'] = o.smote_args['k_neighbors']
+        except: pass
 
-# find all kmeanssmote instances which correspond to the fixed params
-oversampler_names, oversamplers = zip(*binary_experiment.oversamplers_)
-oversamplers_dict = {}
-for i, o in enumerate(oversamplers):
-    try:
-        if o.kmeans_args:
-            oversamplers_dict[oversampler_names[i]] = {}
-            oversamplers_dict[oversampler_names[i]]['k'] = o.kmeans_args['n_clusters']
-            oversamplers_dict[oversampler_names[i]]['de'] = o.density_power
-            oversamplers_dict[oversampler_names[i]]['irt'] = o.imbalance_ratio_threshold
-            oversamplers_dict[oversampler_names[i]]['knn'] = o.smote_args['k_neighbors']
-    except: pass
+    filtered_oversamplers = {}
+    for name, params in oversamplers_dict.items():
+        retain = True
+        for param, value in fixed_params.items():
+            retain = retain & (params[param] == fixed_params[param])
+        if retain: filtered_oversamplers[name] = params
+    relevant_oversampler_names = list(filtered_oversamplers.keys())
+    relevant_oversampler_names
 
-filtered_oversamplers = {}
-for name, params in oversamplers_dict.items():
-    retain = True
-    for param, value in fixed_params.items():
-        retain = retain & (params[param] == fixed_params[param])
-    if retain: filtered_oversamplers[name] = params
-relevant_oversampler_names = list(filtered_oversamplers.keys())
-relevant_oversampler_names
+    # match them on the results to get the sensitivity analysis
+    sensitivity = imbtools.evaluation.calculate_stats(binary_experiment)
 
-# match them on the results to get the sensitivity analysis
-sensitivity = imbtools.evaluation.calculate_stats(binary_experiment)
+    df_out = []
+    for clf in classifiers:
+        sensitivity_clf = sensitivity[
+            (sensitivity['Oversampler'].isin(relevant_oversampler_names))
+            & (sensitivity['Metric'].eq(metric))
+            & (sensitivity['Classifier'].eq(clf))
+        ]
+        df_oversamplers = pd.DataFrame(filtered_oversamplers).transpose()
+        sensitivity_clf = sensitivity_clf.join(df_oversamplers.loc[:,variable_param], 'Oversampler')#.reset_index(drop=True)#.reindex(sensitivity_clf.index)
+        sensitivity_clf[metric_name] = sensitivity_clf['Mean CV score'].round(3).apply(str) + ' ±' + sensitivity_clf['Std CV score'].round(3).apply(str)
+        sensitivity_clf[variable_param] = sensitivity_clf[variable_param].replace(np.inf,100000000).replace(np.nan,200000000)
+        sensitivity_clf[variable_param] = sensitivity_clf[variable_param].astype(variable_param_type)
+        sensitivity_clf[variable_param] = sensitivity_clf[variable_param].replace(100000000, '∞').replace(200000000,'default')
+        sensitivity_clf = sensitivity_clf.drop(['Metric','Classifier','Oversampler', 'Mean CV score', 'Std CV score'], axis=1)
+        sensitivity_clf = sensitivity_clf.set_index(['Dataset',variable_param])
+        df_out.append(sensitivity_clf)
+    number_regex = re.compile(r'[0-9]+')
+    sensitivity = pd.concat(df_out, axis=1, keys=[re.sub(number_regex, '', clf) for clf in classifiers])
+    # if len(classifiers) > 1:
 
-sensitivity = sensitivity[
-    (sensitivity['Oversampler'].isin(relevant_oversampler_names))
-    & (sensitivity['Metric'].eq(metric))
-    & (sensitivity['Classifier'].eq(classifier))
-]
-df_oversamplers = pd.DataFrame(filtered_oversamplers).transpose()
-sensitivity = sensitivity.join(df_oversamplers.loc[:,'k'], 'Oversampler')#.reset_index(drop=True)#.reindex(sensitivity.index)
-sensitivity[metric] = sensitivity['Mean CV score'].round(2).apply(str) + ' ±' + sensitivity['Std CV score'].round(2).apply(str)
-sensitivity = sensitivity.drop(['Metric','Classifier','Oversampler', 'Mean CV score', 'Std CV score'], axis=1)
-sensitivity = sensitivity.set_index(['Dataset','k'])
-sensitivity = sensitivity.sortlevel(0)
-print(sensitivity.to_latex().replace('±','$\pm$ '))
+    sensitivity = sensitivity.sortlevel(0)
+    if as_latex:
+        print(sensitivity.to_latex().replace('±','$\pm$ '))
+    else:
+        return sensitivity
+
+# %%
+sensitivity_analysis(
+    ['LogisticRegression1','GradientBoosting1'],
+    ('geometric_mean_score','G-mean'),
+    ('k', np.int),
+    {
+        'de': None,
+        # 'k': 250,
+        'knn':3,
+        'irt': 1
+    })
+
+# %%
+writer = pd.ExcelWriter('/Users/felix/Desktop/sensitivity.xlsx')
+for metric in ['average_precision','geometric_mean_score','f1']:
+    for clf in ['LogisticRegression1','GradientBoosting1']:
+        print(clf, metric)
+        sensitivity_analysis(
+            [clf],
+            (metric,metric),
+            ('knn', np.int),
+            {
+                'de': None,
+                'k': 250,
+                'irt': 1
+        }).to_excel(
+            writer,
+            '{} {}'.format(clf[:15], metric[:15])
+        )
+writer.save()
+
+# %%
+sensitivity_analysis(
+    ['LogisticRegression1'],
+    ('geometric_mean_score','G-mean'),
+    ('de', np.int),
+    {
+        'knn': 3,
+        'k': 250,
+        'irt': 1
+    })
+
+# %%
+np.unique(best[('geometric_mean_score','Classifier')], return_counts=True)
